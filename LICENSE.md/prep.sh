30bd4d4e921524db0cc2d97b074e6ccdd1f85539
Donny Davis <donny@fortnebula.com>
2018-05-08 08:12:53 -0400

    adding tempest prep script

---
+++ LICENSE.md/prep.sh
@@ -0,0 +1,12 @@
+source ~/overcloudrc
+openstack network create test-network --share
+openstack subnet create test-subnet --subnet-range '192.168.1.0/24' --network test-network
+openstack router create test-router
+openstack router add subnet test-router test-subnet
+openstack network create test-external --external  --provider-network-type flat --provider-physical-network datacentre
+openstack subnet create test-external-subnet --subnet-range 192.168.24.0/24  --gateway 192.168.24.1 --allocation-pool start=192.168.24.200,end=192.168.24.240 --no-dhcp --network test-external
+openstack router set test-router --external-gateway test-external
+sudo yum -y install openstack-tempest python-glance-tests python-keystone-tests python-horizon-tests-tempest python-neutron-tests python-cinder-tests python-nova-tests python-swift-tests python-ceilometer-tests python-gnocchi-tests python-aodh-tests
+tempest init overcloud-test
+cd overcloud-test/
+tempest workspace list
